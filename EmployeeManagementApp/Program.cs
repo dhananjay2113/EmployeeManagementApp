88a510788d74c7234534782185d20d2003846f03
Dhananjay Singh <dsinghpanwar211@gmail.com>
2025-04-26 11:43:04 +0530

    test

--- EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Employee
+{
+    private int ID { get; set; }
+    private string Name { get; set; }
+    private string Address { get; set; }
+}
+class EmployeeManagement
+{
+    Employee List<Employee> = new 
+    static void Main()
+    {
+
+    }
+    static void AddEmployee()
+    {
+        
+    }
+}
