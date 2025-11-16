eba3ec4358f7df41fb67a595e28ea18ca4aee996
Dhananjay S Panwar <dsinghpanwar211@gmail.com>
2025-11-16 13:02:33 +0530

    added EmployeeManagement to tracking

--- EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/Program.cs
@@ -173,7 +173,7 @@ class EmployeeManagement
                 empEdit = employee;
                 break;
             }
-            
+
         }
         if (empEdit != null)
         {
@@ -187,6 +187,23 @@ class EmployeeManagement
         else
         {
             Console.WriteLine("No Employees Found");
+        }
+    }
+
+    private void DisplayDateInDifferentCultures()
+    {
+        DateTime currentDate = DateTime.Now;
+        CultureInfo[] cultures = new CultureInfo[]
+        {
+            new CultureInfo("en-US"), // United States
+            new CultureInfo("fr-FR"), // France
+            new CultureInfo("ja-JP"), // Japan
+            new CultureInfo("ar-SA")  // Saudi Arabia
+        };
+        foreach (var culture in cultures)
+        {
+            string formattedDate = currentDate.ToString(culture);
+            Console.WriteLine($"Date in {culture.Name}: {formattedDate}");
         }
     }
 }
